d01db9f25702479c07db5f51b294211e66eb7367
Lex van Roon <r3boot@r3blog.nl>
2017-12-20 02:36:24 +0100

    Several fixes in consul

--- lib/consul/consul.go
+++ lib/consul/consul.go
@@ -96,6 +96,13 @@ func NewConsul(l *logger.Logger, cfg *config.Config) (*Consul, error) {
 			Name:      fmt.Sprintf("Health check for %s", svcName),
 			Notes:     svcDescr,
 			ServiceId: svcInstance,
+			Definition: CheckDefinitionData{
+				Http:     fmt.Sprintf("http://%s:6548/v1/health", cfg.Service.Address),
+				Method:   http.MethodGet,
+				Interval: "5s",
+				Timeout:  "1s",
+				DeregisterCriticalServiceAfter: "30s",
+			},
 		},
 	}
 
